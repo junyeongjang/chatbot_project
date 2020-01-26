55908ba4a7e79e83769ce9e61ba29382e7bec560
junyeongjang <junyeong.dev@gmail.com>
2020-01-26 15:49:00 +0900

    node js chatbot

---
+++ telegram_chatbot/app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+
+const TelegramBot = require('node-telegram-bot-api'); 
+const token = '1031768450:AAFy8Csn6kabeU9AlKf0VLzOFeB6hwxAmaE';
+const bot = new TelegramBot(token, {polling: true});
+
+
+//onText란 /start와 같은 명령어를 뜻함 
+bot.onText(/\/start/, (msg) => {
+    bot.sendMessage(msg.chat.id, "Ezai 챗봇에 오신 것을 환영합니다.");
+});
+
+// 사용자가 message를 보내면 내가 원하는 메세지를 보냄
+bot.on('message', (msg) => {
+    //msg.text 키워드 
+   console.log(msg.text);
+   bot.sendMessage(msg.chat.id, "Ezai 챗봇에 오신 것을 환영합니다.2");
+   
+
+});
