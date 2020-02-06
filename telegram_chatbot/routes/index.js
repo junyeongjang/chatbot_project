1282e8bc08442a4d69d20cfd766660c689b7de5c
junyeongjang <junyeong.dev@gmail.com>
2020-02-06 18:41:43 +0900

    chatbot update

--- telegram_chatbot/routes/index.js
+++ telegram_chatbot/routes/index.js
@@ -1,18 +1,40 @@
 var express = require('express');
-var User = require('../models').chat;
-
+const Chat = require('../models').Chat;
 var router = express.Router();
 
+const TelegramBot = require('node-telegram-bot-api'); 
+const token2 = '821604164:AAExhkSPWgRv5aDVSh29zSaOJpmyIWHWcK8';
+const bot2 = new TelegramBot(token2, {polling: true});
+
 router.get('/', function(req, res, next) {
-  User.findAll()
-    .then((chat) => {
-      console.log("여기까찌");
-      res.render('main', { chats });
+  res.render('main2.html')
+});
+
+router.post('/chat-post', function(req, res, next) {
+  Chat.create({
+    keyword: req.body.keyword,
+    story: req.body.content,
+  }).
+    then(() =>{
+      console.log("통과");
     })
-    .catch((err) => {
+    .catch((err)=>{
       console.error(err);
       next(err);
     });
+
+    bot2.onText(/\/start/, (msg) => {
+      bot2.sendMessage(msg.chat.id, req.body.keyword);
+    });
+    res.send(req.body.keyword);
 });
 
+// bot2.onText(/\/start/, (msg) => {
+//   bot2.sendMessage(msg.chat.id, "Ezai 챗봇에 오신 것을 환영합니다.");  
+//   bot2.sendMessage(msg.chat.id, "Ezai 챗봇에 오신 것을 환영합니다."); 
+//   //photo 전송 
+//   bot2.sendPhoto(msg.chat.id,"/Users/jangjun-yeong/Documents/GitHub/chatbot_project/telegram_chatbot/logo.png" );
+//   //location 전송 좌표를 전송하면 google 맵에서 좌표 표시
+//   bot2.sendLocation(msg.chat.id,37.602859, 126.955252); //상명대학교 좌표
+// });
 module.exports = router;
--- telegram_chatbot/server.js
+++ telegram_chatbot/server.js
@@ -1,29 +1,38 @@
+//미들웨어 세팅
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
+//라우터 연결
 var indexRouter = require('./routes/index');
+var startChatRouter = require('./routes/startChat');
 
-
-var chatsRouter = require('./routes/chats');
-var sequelize = require('./models').sequelize;
-
+// 익스프레스 연결
 var app = express();
+
+//시퀄라이즈 DB연결
+var sequelize = require('./models').sequelize;
 sequelize.sync();
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 app.set('port',process.env.PORT||8001);
 
+//미들웨어 사용 static, dev
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// 라우터 이동
 app.use('/', indexRouter);
-app.use('/chat', chatsRouter);
+app.use('/chatStart', startChatRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -36,7 +45,6 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
