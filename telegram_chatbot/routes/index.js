24006fcf2218ab9892d37e4ae3bb45ea91854ac1
junyeongjang <junyeong.dev@gmail.com>
2020-05-31 18:55:59 +0900

    Update index.js

--- telegram_chatbot/routes/index.js
+++ telegram_chatbot/routes/index.js
@@ -1,14 +1,46 @@
 var express = require('express');
-const Chat = require('../models').Chat;
 var router = express.Router();
-
-const TelegramBot = require('node-telegram-bot-api'); 
-const token2 = '821604164:AAExhkSPWgRv5aDVSh29zSaOJpmyIWHWcK8';
-const bot2 = new TelegramBot(token2, {polling: true});
-
-router.get('/', function(req, res, next) {
-  res.render('main2.html')
+const {Chat} = require('../models');
+router.get('/', function(req, res, next) { 
+  
+  Chat.findAll({
+    attribute:['keyword', 'story'],
+    where:{id:1}
+  }) 
+  .then((chat) => { 
+    console.log(chat);
+    res.render('index', { 
+      title: 'hi',
+      user: chat
+      });                                                                   
+   }); 
 });
+
+
+
+// router.get('/', function(req, res, next) { 
+//     const cont = Chat.find({where: {id:1}});
+//     console.log(cont);
+//     //   Chat.findAll({
+//     //     where:{id:3}
+//     //   }) 
+//     //   .then((chat) => {
+//     //     console.log(chat);
+//     //     console.log(chat.content);
+//     //     console.log(chat.id);
+//     //     console.log(chat.keyword);
+//     //     res.render('index', { 
+//     //       title: 'Express', 
+//     //       useremail: chat,
+//     //  });
+//   //  res.render('index', { title: 'Express' }); 
+//   // Chat.findAll({
+//   //   where:{
+//   //     id:1
+//   //   }
+//   // });
+// });
+
 
 router.post('/chat-post', function(req, res, next) {
   Chat.create({
@@ -29,12 +61,4 @@ router.post('/chat-post', function(req, res, next) {
     res.send(req.body.keyword);
 });
 
-// bot2.onText(/\/start/, (msg) => {
-//   bot2.sendMessage(msg.chat.id, "Ezai 챗봇에 오신 것을 환영합니다.");  
-//   bot2.sendMessage(msg.chat.id, "Ezai 챗봇에 오신 것을 환영합니다."); 
-//   //photo 전송 
-//   bot2.sendPhoto(msg.chat.id,"/Users/jangjun-yeong/Documents/GitHub/chatbot_project/telegram_chatbot/logo.png" );
-//   //location 전송 좌표를 전송하면 google 맵에서 좌표 표시
-//   bot2.sendLocation(msg.chat.id,37.602859, 126.955252); //상명대학교 좌표
-// });
 module.exports = router;
